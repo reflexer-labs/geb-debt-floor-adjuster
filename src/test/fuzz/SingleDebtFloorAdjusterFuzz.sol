7e4410622fbdc80736e756b5af7fd556ed620199
fabiohild <fabiohildebrand@gmail.com>
2021-03-24 21:31:27 -0300

    wip, integration fuzzing of debt ceiling and floor setters

--- src/test/fuzz/SingleDebtFloorAdjusterFuzz.sol
+++ src/test/fuzz/SingleDebtFloorAdjusterFuzz.sol
@@ -5,6 +5,7 @@ import {OracleRelayer} from "../../../lib/geb/src/OracleRelayer.sol";
 import "../Mock/MockTreasury.sol";
 
 import "./SingleDebtFloorAdjusterMock.sol";
+import {SingleSpotDebtCeilingSetterMock} from "./SingleSpotDebtCeilingSetterMock.sol";
 
 contract OracleMock {
     uint result;
@@ -167,4 +168,152 @@ contract Fuzz is SingleDebtFloorAdjusterMock {
         (,,,, uint256 debtFloor) = safeEngine.collateralTypes(collateralName);
         return (debtFloor >= minDebtFloor && debtFloor <= maxDebtFloor) || lastUpdateTime == 0;
     }
+}
+
+contract SAFEEngineMock is SAFEEngine {
+    function modifyFuzzParameters(
+        bytes32 collateralType,
+        bytes32 parameter,
+        uint256 data
+    ) external isAuthorized {
+        if (parameter == "debtAmount") collateralTypes[collateralType].debtAmount = data;
+        else if (parameter == "accumulatedRate") collateralTypes[collateralType].accumulatedRate = data;
+        else revert();
+    }
+}
+
+// Integration Fuzz
+contract IntegrationFuzz {
+    TokenMock token;
+    SAFEEngineMock safeEngine;
+    OracleRelayer oracleRelayer;
+    MockTreasury treasury;
+    OracleMock gasPriceOracle;
+    OracleMock ethPriceOracle;
+    SingleDebtFloorAdjusterMock floorAdjuster;
+
+    bytes32 collateralName = "ETH";
+    uint baseUpdateCallerReward          = 5 ether;
+    uint maxUpdateCallerReward           = 10 ether;
+    uint perSecondCallerRewardIncrease   = 1000192559420674483977255848; // 100% per hour;
+    uint updateDelay                     = 1 hours;
+    uint maxRewardIncreaseDelay          = 6 hours;
+    uint gasAmountForLiquidation         = 6000000;
+    uint maxDebtFloor                    = 1000    * 10**45; // 1k
+    uint minDebtFloor                    = 100     * 10**45; // 100
+    uint debtCeiling                     = 1000000 * 10**45; // 1m
+    uint debtFloor                       = 800     * 10**45;
+
+    SingleSpotDebtCeilingSetterMock ceilingSetter;
+
+    uint256 ceilingPercentageChange      = 120;
+    uint256 maxCollateralCeiling         = 1000    * 10**45; // 1k
+    uint256 minCollateralCeiling         = 1       * 10**45; // 1
+
+    constructor() public {
+
+        token = new TokenMock();
+        safeEngine = new SAFEEngineMock();
+        oracleRelayer = new OracleRelayer(address(safeEngine));
+        treasury = new MockTreasury(address(token));
+        gasPriceOracle = new OracleMock(120000000000);  // 120 gwei
+        ethPriceOracle = new OracleMock(5000 * 10**18); // 5k
+
+        floorAdjuster = new SingleDebtFloorAdjusterMock(
+            address(safeEngine),
+            address(oracleRelayer),
+            address(treasury),
+            address(gasPriceOracle),
+            address(ethPriceOracle),
+            collateralName,
+            baseUpdateCallerReward,
+            maxUpdateCallerReward,
+            perSecondCallerRewardIncrease,
+            updateDelay,
+            gasAmountForLiquidation,
+            maxDebtFloor,
+            minDebtFloor
+        );
+
+        ceilingSetter = new SingleSpotDebtCeilingSetterMock(
+            address(safeEngine),
+            address(oracleRelayer),
+            address(treasury),
+            collateralName,
+            baseUpdateCallerReward,
+            maxUpdateCallerReward,
+            perSecondCallerRewardIncrease,
+            updateDelay,
+            ceilingPercentageChange,
+            maxCollateralCeiling,
+            minCollateralCeiling
+        );
+
+        safeEngine.modifyParameters(collateralName, "debtCeiling", debtCeiling);
+        safeEngine.modifyParameters(collateralName, "debtFloor", debtFloor);
+
+
+        safeEngine.initializeCollateralType(collateralName);
+        safeEngine.addAuthorization(address(floorAdjuster));
+        safeEngine.addAuthorization(address(ceilingSetter));
+        oracleRelayer.modifyParameters("redemptionPrice", 3.14 ether);
+        floorAdjuster.modifyParameters("maxRewardIncreaseDelay", 5 hours);
+        ceilingSetter.modifyParameters("maxRewardIncreaseDelay", 5 hours);
+    }
+
+    modifier recompute() {
+        _;
+        floorAdjuster.recomputeCollateralDebtFloor(address(0xfab));
+        // ceilingSetter.autoUpdateCeiling(address(0xfab));
+    }
+
+    function notNull(uint val) internal returns (uint) {
+        return val == 0 ? 1 : val;
+    }
+
+    function maximum(uint a, uint b) internal returns (uint) {
+        return (b >= a) ? b : a;
+    }
+
+    function fuzzEthPrice(uint ethPrice) public recompute {
+        OracleMock(address(ethPriceOracle)).setPrice(notNull(ethPrice % 1000 ether)); // up to 100k
+    }
+
+    function fuzzGasPrice(uint gasPrice) public recompute {
+        OracleMock(address(gasPriceOracle)).setPrice(notNull(gasPrice % 10000000000000)); // up to 10000 gwei
+    }
+
+    function fuzzGasAmountForLiquidation(uint gasAmountForLiquidation) public recompute {
+        floorAdjuster.modifyParameters("gasAmountForLiquidation", notNull(gasAmountForLiquidation % block.gaslimit)); // up to block gas limit
+    }
+
+    function fuzzRedemptionPrice(uint redemptionPrice) public recompute {
+        OracleRelayer(address(oracleRelayer)).modifyParameters("redemptionPrice", maximum(redemptionPrice % 10**39, 10**24));
+    }
+
+    function fuzzDebtAmount(uint debtAmount) public recompute {
+        safeEngine.modifyFuzzParameters(collateralName, "debtAmount", debtAmount);
+    }
+
+    function fuzzAccumulatedRate(uint accumulatedRate) public recompute {
+        safeEngine.modifyFuzzParameters(collateralName, "accumulatedRate", accumulatedRate);
+    }
+
+    // properties
+    function echidna_debt_floor() public returns (bool) {
+        (,,,, uint256 debtFloor,) = safeEngine.collateralTypes(collateralName);
+        return (debtFloor == floorAdjuster.getNextCollateralFloor() || floorAdjuster.lastUpdateTime() == 0);
+    }
+
+    function echidna_debt_floor_bounds() public returns (bool) {
+        (,,,, uint256 debtFloor,) = safeEngine.collateralTypes(collateralName);
+        return (debtFloor >= floorAdjuster.minDebtFloor() && debtFloor <= floorAdjuster.maxDebtFloor()) || floorAdjuster.lastUpdateTime() == 0;
+    }
+
+    function echidna_debt_floor_lower_than_debt_ceiling() public returns (bool) {
+        (,,, uint256 debtCeiling, uint256 debtFloor,) = safeEngine.collateralTypes(collateralName);
+        return (debtCeiling > debtFloor);
+    }
+
+    // (uint256 debtAmount, uint256 accumulatedRate,, uint256 currentDebtCeiling, uint256 debtFloor,) = safeEngine.collateralTypes(collateralName);
 }
